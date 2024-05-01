d595c9003eb2e5f3ea5e749d3b0fc811616e9508
Isam Ibrahim <isam@cprosolutions.com.my>
2024-05-01 09:46:11 -1000

    Added maf.gz concatenator

---
+++ DNAseq-short-variant-discovery/helper_scripts/utility/maf_concatenator_gz.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# get project name
+# get maf dir 
+# get number of lines to skip
+while getopts m:n:o:p: flag
+do
+    case "${flag}" in
+        m) maf_dir=${OPTARG};;
+        n) lines_to_skip=${OPTARG};;
+        o) output_dir=${OPTARG};;
+        p) project_name=${OPTARG};;
+    esac
+done
+
+mkdir -p ${output_dir}
+
+# Funcotated MAFs usually have 160 lines to skip
+# aggregates all mafs together into a temp file
+find ${maf_dir} -type f -name "*maf.gz" | xargs -I % zcat % | tail -n+${lines_to_skip} >> ${output_dir}/${project_name}.aggregated.maf.tmp
+# removes all but the first header
+sed '1!{/^Hugo/d;};' ${output_dir}/${project_name}.aggregated.maf.tmp > ${output_dir}/${project_name}.aggregated.maf
+# remove temp file
+rm ${output_dir}/${project_name}.aggregated.maf.tmp
+gzip ${output_dir}/${project_name}.aggregated.maf
